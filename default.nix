5333969f63308be2bf5b522fb3f530171d3ef01d
zupaPolina <nesovsem@tuta.io>
2021-03-06 17:17:53 +0300

    fix3

--- default.nix
+++ default.nix
@@ -1,21 +1,14 @@
-let requiredVersion = import ./lib/minver.nix; in
+# This file describes your repository contents.
+# It should return a set of nix derivations
+# and optionally the special attributes `lib`, `modules` and `overlays`.
+# It should NOT import <nixpkgs>. Instead, you should take pkgs as an argument.
+# Having pkgs default to <nixpkgs> is fine though, and it lets you use short
+# commands such as:
+#     nix-build -A mypackage
 
-if ! builtins ? nixVersion || builtins.compareVersions requiredVersion builtins.nixVersion == 1 then
+{ pkgs ? import <nixpkgs> {} }:
 
-  abort ''
-    This version of Nixpkgs requires Nix >= ${requiredVersion}, please upgrade:
-    - If you are running NixOS, `nixos-rebuild' can be used to upgrade your system.
-    - Alternatively, with Nix > 2.0 `nix upgrade-nix' can be used to imperatively
-      upgrade Nix. You may use `nix-env --version' to check which version you have.
-    - If you installed Nix using the install script (https://nixos.org/nix/install),
-      it is safe to upgrade by running it again:
-          curl https://nixos.org/nix/install | sh
-    For more information, please see the NixOS release notes at
-    https://nixos.org/nixos/manual or locally at
-    ${toString ./nixos/doc/manual/release-notes}.
-    If you need further help, see https://nixos.org/nixos/support.html
-  ''
+{
+  foliate = pkgs.callPackage ./pkgs/foliate { };
+}
 
-else
-
-  import ./pkgs/top-level/impure.nix
